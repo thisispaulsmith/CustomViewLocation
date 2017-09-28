21d664d0d4b911de2f3f63a0fc8264f5369f7f8a
Paul Smith <paul.smith@communicatorcorp.com>
2017-09-28 10:48:39 +0100

    Initial commit

---
+++ src/NetSmith.AspNetCore.CustomViewLocation/FeaturesViewLocationExpander.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+
+namespace Communicator.AspNetCore.Mvc
+{
+    /// <summary>
+    /// Custom view locator, view in feature folder
+    /// </summary>
+    public class FeaturesViewLocationExpander : IViewLocationExpander
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            context.Values["customviewlocation"] = nameof(FeaturesViewLocationExpander);
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            var viewLocationFormats = new[]
+            {
+                "~/Features/{1}/{0}.cshtml",
+                "~/Features/_Shared/{0}.cshtml"
+            };
+
+            return viewLocationFormats;
+        }
+    }
+}
---
+++ src/NetSmith.AspNetCore.CustomViewLocation/FeaturesViewLocationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Communicator.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FeaturesViewLocationExtensions
+    {
+        public static void AddCustomViewLocator(this IServiceCollection services)
+        {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new FeaturesViewLocationExpander());
+            });
+        }
+    }
+}
